6e65c140df3b66892b6e9585cc21ee3a4f6cd454
Chirag Garg <102269022+cgarg3@users.noreply.github.com>
2022-09-22 17:12:16 -0400

    Add files via upload

---
+++ milestone3/Streamable.cpp
@@ -0,0 +1,19 @@
+#include "Streamable.h"
+
+namespace sdds {
+   // Empty virtual destructor to this interface to guaranty that the descendants of the Streamable are removed from memory with no leak
+   Streamable::~Streamable() {};
+
+   // Insertion and Extraction operator overloads
+   std::ostream& operator<<(std::ostream& os, const Streamable& s) {
+      // Object of type Streamable can be written on an ostream object only if the Streamable object is in a valid state
+      if (s) {
+         s.write(os);
+      }
+      return os;
+   }
+
+   std::istream& operator>>(std::istream& is, Streamable& s) {
+      return (s.read(is));
+   }
+}
